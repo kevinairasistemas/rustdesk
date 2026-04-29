1d5b82ecdbf05d6dc563e0c8f3d72236b234400c
kevinairasistemas <kevinairasistemas@gmail.com>
2026-04-29 12:14:20 +0200

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -1819,7 +1819,7 @@ pub async fn get_key(sync: bool) -> String {
         options.remove("key").unwrap_or_default()
     };
     if key.is_empty() {
-        key = config::RS_PUB_KEY.to_owned();
+        key = config::dJ1fZrcmgADYHKrN0Qq7Bi7QDDnTlc50AMoq3pXxE3c=.to_owned();
     }
     key
 }
