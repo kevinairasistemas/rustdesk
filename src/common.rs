07795dbedc2444c34938376fde20e0ec6f52abe8
kevinairasistemas <kevinairasistemas@gmail.com>
2026-04-30 11:20:55 +0200

    Update key assignment to use string literal

--- src/common.rs
+++ src/common.rs
@@ -1823,7 +1823,7 @@ pub async fn get_key(sync: bool) -> String {
         options.remove("key").unwrap_or_default()
     };
     if key.is_empty() {
-        key = config::dJ1fZrcmgADYHKrN0Qq7Bi7QDDnTlc50AMoq3pXxE3c=.to_owned();
+        key = "dJ1fZrcmgADYHKrN0Qq7Bi7QDDnTlc50AMoq3pXxE3c=".to_owned();
     }
     key
 }
